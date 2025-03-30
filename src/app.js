4baf6c87b9f765a030e8f71f27d204036d4b57bc
Maria Alejandra Robles <alejita-robles@hotmail.com>
2025-03-30 17:51:03 +0000

    domain project finished

--- src/app.js
+++ src/app.js
@@ -5,7 +5,21 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our', 'ours', 'my'];
+let adj = ['greatest', 'big', 'almighty', 'biggest', 'powerful'];
+let noun = ['dj', 'alestyx', 'programmer', 'music', 'codefontain'];
+let extensions = ['.com', '.es', '.dnb', '.co']
+
+function getRandomIndex (length) {
+  return Math.floor(Math.random()* (length - 1))
+}
+
+function getRandomElements (anyArray) {
+  let random = getRandomIndex (anyArray.length);
+  return anyArray[random]
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+ const randomDomain = document.getElementById('myDomain')
+ randomDomain.innerHTML = getRandomElements(pronoun)+ getRandomElements(adj) + getRandomElements(noun) + getRandomElements(extensions)
+
 };
